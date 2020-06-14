9f2470bc76bdbb56dd3a55d52715471961dd3bec
Marc Erhardtsen <marcerhardtsen@MacBook-Pro.local>
2020-06-14 11:14:48 +0200

    persisting theme state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const themeHook = useState("peru");
   return (
     // Passing the whole hook through to the theme context
+    // Important thing about using Context here is that app is always rendered so this state will persist beyond page navigation
     <ThemeContext.Provider value={themeHook}>
       <div>
         <header>
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import Results from "./Results";
 import useDropdown from "./useDropdown";
-import ThemeContext from "./ThemeContext"
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
@@ -12,7 +12,7 @@ const SearchParams = () => {
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   // Note that context does not have to be a hook, can be object or function or whatever you want
   // In this case we only want the theme not the updater function
-  const [theme] = useContext(ThemeContext)
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     setBreeds([]);
@@ -53,7 +53,21 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button style={{background: theme}}>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            name="themeSelect"
+            id="theme"
+            onBlur={e => setTheme(e.target.value)}
+            onChange={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="green">Green</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ background: theme }}>Submit</button>
       </form>
       <Results pets={pets}></Results>
     </div>
